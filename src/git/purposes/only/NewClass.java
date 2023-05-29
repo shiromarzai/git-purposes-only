3f2b38f4e1273d81b78005292994b0a55127e62a
shiromarzai <shahryar_omarzai@hotnmail.com>
2023-05-28 17:05:35 -0700

    last class

---
+++ src/git/purposes/only/NewClass.java
@@ -0,0 +1,10 @@
+package git.purposes.only;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("first class");
+	}
+
+}
