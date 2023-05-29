efb5f030f09e6747abeba36d57a116b8fdeb4911
shiromarzai <shahryar_omarzai@hotnmail.com>
2023-05-28 17:00:18 -0700

    final part

--- src/git/purposes/only/ForGitPurposesOnly.java
+++ src/git/purposes/only/ForGitPurposesOnly.java
@@ -5,6 +5,7 @@ public class ForGitPurposesOnly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This time From git Hub");
+System.out.println("another file added");
 	}
 
 }
